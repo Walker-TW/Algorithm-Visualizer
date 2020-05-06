f8c11dc084e250889315189e1d6f449c828a2f12
basselalsayed <alsayed.bassel@gmail.com>
2020-05-06 19:41:39 +0100

    created grid, node components

---
+++ src/Grid/Grid.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Node from './Nodes/Node';
+
+import './Grid.css';
+
+export default class Grid extends Component {
+  gridSetup = () => {
+    let nodes = [];
+    for (let row = 0; row < 51; row++) {
+      let current_row = [];
+      for (let col = 0; col < 30; col++) {
+        current_row.push([]);
+      }
+      nodes.push(current_row);
+    }
+    return nodes;
+  };
+
+  render() {
+    return (
+      <div className="Grid">
+        {this.gridSetup().map((row) => {
+          return (
+            <div className="Row">
+              {row.map((col) => (
+                <Node />
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
---
+++ src/Grid/Nodes/Node.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Node = () => {
+  const getStyle = {
+    width: '20px',
+    height: '20px',
+    border: '1px solid black',
+  };
+
+  return <div style={getStyle} />;
+};
+
+export default Node;
