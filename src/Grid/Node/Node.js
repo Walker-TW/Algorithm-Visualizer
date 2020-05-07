4d615b716e2386ee2bff973affc7fef23d8b15e9
Walker-TW <thomas.w.walker@live.co.uk>
2020-05-07 12:55:43 +0100

    Added start and End nodes that change states and color"

--- src/Grid/Node/Node.js
+++ src/Grid/Node/Node.js
@@ -1,8 +1,58 @@
-import React, { Component } from 'react';
-import './Node.css';
+import React, { Component } from "react";
+import "./Node.css";
 
 export default class Node extends Component {
+  state = {
+    start: false,
+    finish: false,
+    visited: false,
+  };
+
+  getClass = () => {
+    const { start, finish, visited } = this.state;
+
+    if (start) {
+      return " Start";
+    } else if (finish) {
+      return " Finish";
+    } else if (visited) {
+      return " Visited";
+    } else {
+      return "";
+    }
+  };
+
+  clickHandler = () => {
+    const { start, finish, visited } = this.state;
+
+    if (!start && !finish) {
+      this.setState({ start: true });
+    } else if (start && !finish) {
+      this.setState({ start: false, finish: true });
+    }
+  };
+
+  downHandler = () => {
+    const { start, finish, visited } = this.state;
+
+    if (!start && !finish) {
+      this.setState({ start: true });
+    } else if (start && !finish) {
+      this.setState({ finish: false });
+    } else if (finish && !start) {
+      this.setState({ start: false, finish: false });
+    }
+  };
+
   render() {
-    return <div className="Node Visited"></div>;
+    return (
+      <div
+        // onMouseDown={() => this.downHandler()}
+        onClick={() => this.clickHandler()}
+        className={"Node" + this.getClass()}
+      ></div>
+    );
   }
 }
+
+//
