9ebf37f9ae6909b3afa393349fc4d611d20970a4
Walker-TW <thomas.w.walker@live.co.uk>
2020-05-08 13:22:00 +0100

    Added App check test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("App renders", () => {
+  const component = renderer.create(<App />).toJSON();
+  expect(component).toMatchSnapshot();
 });
