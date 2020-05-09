38d25a81846a210a28db110846c8a21158aecac9
Walker-TW <thomas.w.walker@live.co.uk>
2020-05-09 18:26:15 +0100

    Created tests to check states of Nodes and that they are rendered

---
+++ src/Grid/Node/Node.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./../../App";
+import Grid from "./../Grid";
+import Node from "./Node";
+import { shallowToJson } from "enzyme-to-json";
+
+import { shallow, mount, render } from "enzyme";
+
+test("Node renders", () => {
+  const component = renderer.create(<Node />).toJSON();
+
+  expect(component).toMatchSnapshot();
+});
+
+describe("node", () => {
+  const output = shallow(<Node />);
+  it("should render with a sate of false", () => {
+    const output = shallow(<Node />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it("starts with required states", () => {
+    expect(output.state().start).toEqual(false);
+    expect(output.state().finish).toEqual(false);
+    expect(output.state().distance).toEqual(Infinity);
+    expect(output.state().visited).toEqual(false);
+  });
+});
