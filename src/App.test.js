9a03c0dbe7c705ba1cdd92afe2b543608eb707eb
Walker-TW <thomas.w.walker@live.co.uk>
2020-05-10 16:15:21 +0100

    cleaned up dead code

--- src/App.test.js
+++ src/App.test.js
@@ -14,10 +14,3 @@ describe("Addition", () => {
     expect(2 + 2).toBe(4);
   });
 });
-
-// what to test
-// - that the nodes are in the grid AND/OR that the grid has rendered? XX
-// - that the states of the nodes are what we want at the beginning XXX?
-// - that all nodes are rendered with the Beginnning  state XXX
-// - that the nodes change on click (as well as the state of the nodes as well)
-// - that the start/finish nodes change when multiple clicked
