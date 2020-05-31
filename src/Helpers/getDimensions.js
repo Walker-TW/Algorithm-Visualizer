d9d178b23fd62d03814551cd04fe1b9a020fae8a
basselalsayed <alsayed.bassel@gmail.com>
2020-05-31 12:51:25 +0100

    fine tuned grid size based on device ie navbar height

--- src/Helpers/getDimensions.js
+++ src/Helpers/getDimensions.js
@@ -1,14 +1,54 @@
 const [screenWidth, screenHeight] = [window.innerWidth, window.innerHeight];
-const PADDING = 40;
+const GRID_PADDING = 40;
+const FULL_SIZE_NAV_HEIGHT = 48;
+const MID_SIZE_NAV_HEIGHT = 104;
+const THIN_SIZE_NAV_HEIGHT = 104;
+const STATS_HEIGHT = 83;
+
 let width, height;
+
+const fullSize = screenWidth > 1654;
+const midSize = screenWidth > 1212;
+const thinSize = screenWidth > 991;
+
+const tablet = screenWidth > 414;
 
 export const getDimensions = (nodeSize) => {
   let size = nodeSize ? nodeSize : defaultNodeSize();
-
-  [width, height] = [
-    Math.floor((screenWidth - PADDING) / size),
-    Math.floor((screenHeight - PADDING) / size),
-  ];
+  if (fullSize) {
+    console.log('fullsize');
+    [width, height] = [
+      Math.floor((screenWidth - GRID_PADDING) / size),
+      Math.floor(
+        (screenHeight - GRID_PADDING - FULL_SIZE_NAV_HEIGHT - STATS_HEIGHT) /
+          size
+      ),
+    ];
+  } else if (midSize) {
+    console.log('mid');
+    [width, height] = [
+      Math.floor((screenWidth - GRID_PADDING) / size),
+      Math.floor(
+        (screenHeight - GRID_PADDING - MID_SIZE_NAV_HEIGHT - STATS_HEIGHT) /
+          size
+      ),
+    ];
+  } else if (thinSize) {
+    console.log('thin');
+    [width, height] = [
+      Math.floor((screenWidth - GRID_PADDING) / size),
+      Math.floor(
+        (screenHeight - GRID_PADDING - THIN_SIZE_NAV_HEIGHT - STATS_HEIGHT) /
+          size
+      ),
+    ];
+  } else {
+    console.log('tab mobile');
+    [width, height] = [
+      Math.floor((screenWidth - GRID_PADDING) / size),
+      Math.floor((screenHeight - GRID_PADDING) / size),
+    ];
+  }
 
   return [width, height];
 };
@@ -16,13 +56,10 @@ export const getDimensions = (nodeSize) => {
 export const defaultNodeSize = () => {
   let nodeSize;
 
-  if (screenWidth > 1450) {
-    nodeSize = 20;
-  } else if (screenWidth > 900) {
-    nodeSize = 30;
-  } else {
-    nodeSize = 40;
-  }
+  if (thinSize) nodeSize = 20;
+  else if (tablet) nodeSize = 30;
+  else nodeSize = 40;
+
   return nodeSize;
 };
 
--- src/Visualizer/Visualizer.test.js
+++ src/Visualizer/Visualizer.test.js
@@ -34,7 +34,7 @@ describe('<Visualizer />', () => {
   it('renders all 1500 Node components', () => {
     const wrapper = mount(<Visualizer />);
     // expect(wrapper.find(Node)).toEqual(true);
-    expect(wrapper.find('.Node').length).toEqual(768);
+    expect(wrapper.find('.Node').length).toEqual(1323);
   });
 
   it('render a node that will change the start state when clicked', () => {
@@ -54,11 +54,11 @@ describe('<Visualizer />', () => {
     expect(wrapper.state().start.present).toEqual(true);
   });
 
-  it('renders 768 nodes on mobile', () => {
+  it('renders 1323 nodes on mobile', () => {
     window.resizeTo(375, 667);
 
     const wrapperTwo = mount(<Visualizer />);
-    expect(wrapperTwo.find('.Node').length).toEqual(768);
+    expect(wrapperTwo.find('.Node').length).toEqual(1323);
   });
 
   it('registers if a mouse is held', () => {
