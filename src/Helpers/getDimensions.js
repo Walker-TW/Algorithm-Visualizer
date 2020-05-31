12e700e2b9c59bcc72a361ab342917a5109e2e90
basselalsayed <alsayed.bassel@gmail.com>
2020-05-31 10:28:52 +0100

    getMax function deprecated

--- src/Helpers/getDimensions.js
+++ src/Helpers/getDimensions.js
@@ -13,27 +13,6 @@ export const getDimensions = (nodeSize) => {
   return [width, height];
 };
 
-export const getMax = () => {
-  if (screenWidth > 1450) {
-    [width, height] = [
-      Math.floor((screenWidth - 40) / 20),
-      Math.floor((screenHeight - 40) / 20),
-    ];
-  } else if (screenWidth > 900) {
-    [width, height] = [
-      Math.floor((screenWidth - 40) / 30),
-      Math.floor((screenHeight - 40) / 30),
-    ];
-  } else {
-    [width, height] = [
-      Math.floor((screenWidth - 40) / 40),
-      Math.floor((screenHeight - 40) / 40),
-    ];
-  }
-  // return [width, height];
-  return [400, 400];
-};
-
 export const defaultNodeSize = () => {
   let nodeSize;
 
